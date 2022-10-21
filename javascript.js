e173271e6b3e205609c496ab7cd11bcc9bac621b
BrinLewis <brinsley22@hotmail.co.uk>
2022-10-21 13:38:10 +0100

    Create eventListener to make gridBlock black on mouseover Make gridBlock amount vary depending on row size

--- javascript.js
+++ javascript.js
@@ -1,8 +1,26 @@
 sketchpad = document.querySelector("#sketchpad");
 
-for (let i = 0; i < 256; i++) {
-  gridDivs = document.createElement("div");
+let blocksPerRow = 16;
+
+let numOfBlocks = blocksPerRow * blocksPerRow;
+
+for (let i = 0; i < numOfBlocks; i++) {
+  const gridDivs = document.createElement("div");
   sketchpad.appendChild(gridDivs);
   gridDivs.setAttribute("id", "gridBlock-" + (i + 1));
   gridDivs.classList.add("gridBlocks");
-}
+};
+
+const allGridBlocks = document.querySelectorAll(".gridBlocks");
+
+allGridBlocks.forEach((gridBlock) => {
+  gridBlock.addEventListener("mouseover", () => {
+    gridBlock.style.backgroundColor = "black";
+  });
+});
+
+for (let i = 0; i < numOfBlocks; i++) {
+  let gridBlock = document.querySelector(`#gridBlock-${i + 1}`)
+  gridBlock.setAttribute("style", `width: ${500 / blocksPerRow}px; 
+    height: ${500 / blocksPerRow}px`); // 500 = total width(px) of the sketchpad.
+};
