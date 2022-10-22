424f197eada39171091921f45fbcf9c686933150
BrinLewis <brinsley22@hotmail.co.uk>
2022-10-22 15:16:48 +0100

    Make reset and grid size buttons functional Moved box event listeners into createBoxes function so that newly created boxes will also have event listeners applied to them, not just the original boxes

--- javascript.js
+++ javascript.js
@@ -4,18 +4,38 @@ let BoxesPerRow = 16; // Default value
 
 let numOfBoxes = BoxesPerRow * BoxesPerRow;
 
-for (let i = 0; i < numOfBoxes; i++) { //Loop to create an inputted number of boxes
-  const gridDivs = document.createElement("div");
-  sketchpad.appendChild(gridDivs);
-  gridDivs.setAttribute("id", "gridBox-" + (i + 1));
-  gridDivs.classList.add("gridBoxes");
+let sketchpadSize = 600;
+
+sketchpad.setAttribute("style", `width: ${sketchpadSize}px; height: ${sketchpadSize}px;`)
+
+function createBoxes(numOfBoxes) {
+  for (let i = 0; i < numOfBoxes; i++) { //Loop to create an inputted number of boxes
+    const gridDiv = document.createElement("div");
+    sketchpad.appendChild(gridDiv);
+    gridDiv.setAttribute("id", "gridDiv-" + (i + 1));
+    gridDiv.classList.add("gridBoxes");
+    gridDiv.setAttribute("style", `width: ${sketchpadSize / BoxesPerRow}px; 
+    height: ${sketchpadSize / BoxesPerRow}px`)
+
+    gridDiv.addEventListener("mousemove", () => {
+      if (isMouseDown) {
+        gridDiv.style.backgroundColor = "black";
+      }
+    });
+  
+    gridDiv.addEventListener("click", () => {
+      gridDiv.style.backgroundColor = "black";
+    })
+  }
 };
 
-const allGridBoxes = document.querySelectorAll(".gridBoxes");
+createBoxes(numOfBoxes);
+
+const allGridDivs = document.querySelectorAll(".gridBoxes");
 
 let isMouseDown = false;
 
-window.addEventListener("mousedown", function (event) { 
+window.addEventListener("mousedown", function (event) {
   isMouseDown = true;
 });
 
@@ -24,29 +44,12 @@ window.addEventListener("mouseup", function (event) {
 });
 //isMouseDown makes it so that you can only draw on the sketchpad while the mouse is held down.
 
-allGridBoxes.forEach((gridBox) => {
-  gridBox.addEventListener("mousemove", () => {
-    if (isMouseDown) {
-      gridBox.style.backgroundColor = "black";
-    }
-  });
+function clearGrid() {
+  gridBoxes = document.querySelectorAll(".gridBoxes")
+  gridBoxes.forEach(gridBox => gridBox.remove())
+}
 
-  gridBox.addEventListener("click", () => {
-    gridBox.style.backgroundColor = "black";
-  })
-});
-
-let sketchpadSize = 600;
-
-sketchpad.setAttribute("style", `width: ${sketchpadSize}px; height: ${sketchpadSize}px;`)
-
-for (let i = 0; i < numOfBoxes; i++) {
-  let gridBox = document.querySelector(`#gridBox-${i + 1}`)
-  gridBox.setAttribute("style", `width: ${sketchpadSize / BoxesPerRow}px; 
-    height: ${sketchpadSize / BoxesPerRow}px`);
-};
-// Sets the size of each box to fit perfectly within the sketchpad
-
+let gridBoxes = document.querySelectorAll(".gridBoxes")
 const btnContainer = document.querySelector("#btnContainer");
 btnContainer.setAttribute("style", `width: ${sketchpadSize}px;`);
 
@@ -54,14 +57,26 @@ const gridBtn = document.querySelector("#gridBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
 gridBtn.addEventListener("click", () => {
-  let newGridSize = parseInt(prompt("What size grid would you like? Max: 80"));
+  let newGridSize = parseInt(prompt("What size grid would you like? Max: 50"));
 
-  if(isNaN(newGridSize)) {
+  if (isNaN(newGridSize)) {
     return alert("That is not a valid number");
-  } else if (newGridSize > 80) {
+  } else if (newGridSize > 50) {
     return alert("Your number was above the specified amount");
   } else if (newGridSize < 1) {
     return alert("How can a grid have less than one square in it?")
+  } else {
+    clearGrid();
+    BoxesPerRow = newGridSize;
+    numOfBoxes = BoxesPerRow * BoxesPerRow;
+    createBoxes(numOfBoxes);
   }
-  
+
+});
+
+resetBtn.addEventListener("click", () => {
+  gridBoxes = document.querySelectorAll(".gridBoxes")
+  gridBoxes.forEach((gridBox) => {
+    gridBox.style.backgroundColor = "white";
+  });
 });
